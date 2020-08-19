d6f4fcc24ab2db8c646654651ebdb2c48566b9a7
cjyabraham <cjyabraham@gmail.com>
2020-08-19 15:36:42 +0800

    get the column names to match the data files; only pull in all columns from first input file

--- reporting/reporter.py
+++ reporting/reporter.py
@@ -6,21 +6,20 @@ import pandas
 from reporting.utils import get_project_root
 
 
-KEY_COLUMN = 'Email'
-COLUMNS_IN_ORDER = ['First Name', 'Last Name', 'Job Title', 'Company Name',
-                    KEY_COLUMN, 'Country', 'City', 'State/Province', 'Postal Code',
-                    'Address 1', 'Address 2', 'Address 3', 'Company Size', 'Job Function',
-                    'Industry']
+KEY_COLUMN = 'Email_Address'
+COLUMNS_IN_ORDER = ['First_Name', 'Last_Name', 'Job_Title', 'Company_Name',
+                    KEY_COLUMN, 'Country', 'City', 'State_-_Prov', 'Postal_Code',
+                    'Address', 'Address_2', 'Company_Size', 'Job_Function', 'Industry']
 
 
 def generate_report(config, input_filenames, output_csv_filename):
     click.echo('Reading input files: ' + ', '.join(input_filenames))
 
     # calculate a data field containing a subset of emails in all input files
-    master_df = read_file(input_filenames.pop(0))
+    master_df = read_first_file(input_filenames.pop(0))
 
     for input_filename in input_filenames:
-        df = read_file(input_filename)
+        df = read_next_file(input_filename)
 
         master_df = master_df[master_df[KEY_COLUMN].isin(df[KEY_COLUMN])]
 
@@ -36,13 +35,14 @@ def generate_report(config, input_filenames, output_csv_filename):
     master_df.to_csv(output_csv_filename)
 
 
-def read_file(filename):
-    df = pandas.read_csv(filename, names=COLUMNS_IN_ORDER,
-                         header=0, usecols=COLUMNS_IN_ORDER)
+def read_first_file(filename):
+    df = pandas.read_csv(filename, header=0, usecols=COLUMNS_IN_ORDER)
 
-    if get_file_extension(filename) == '.csv':
-        df = pandas.read_csv(filename, names=COLUMNS_IN_ORDER,
-                             header=0, usecols=COLUMNS_IN_ORDER)
+    return df.drop_duplicates(subset=KEY_COLUMN)
+
+
+def read_next_file(filename):
+    df = pandas.read_csv(filename, header=0, usecols=[KEY_COLUMN])
 
     return df.drop_duplicates(subset=KEY_COLUMN)
 
